c44b6c78448ed1b22f52fed61f04a326a0dfc1e9
idk-bored <guythta@gmail.com>
2026-09-13 08:29:36 -0500

    Add Class to Help with Autonomous
    probably never use

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/moveDistanceBasedTime.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+class moveDistanceBasedTime{
+
+    private double currentDistance;
+    private double currentSpeed;
+    private boolean dispatchedTask = false;
+    private double currentMotorSpeed;
+    private double timeNeeded;
+    private ElapsedTime mainTimer = new ElapsedTime();
+    private DcMotorEx[] wheels;
+
+    public moveDistanceBasedTime(DcMotorEx[] wheels){
+        this.wheels = wheels;
+    }
+
+    public void dispatchNewTask(double distance, double speed, double motorSpeed){
+        dispatchedTask = true;
+        currentDistance = distance;
+        currentSpeed = speed;
+        currentMotorSpeed = motorSpeed;
+        try{
+            timeNeeded = distance/(speed*motorSpeed);
+        } finally{
+            timeNeeded = 0;
+        }
+        mainTimer.reset();
+    }
+
+    public void movemotor(){
+        if(!dispatchedTask){
+            return;
+        }
+
+        if(mainTimer.seconds() > timeNeeded){
+            dispatchedTask = false;
+            for(int i = 0; i < wheels.length; ++i){
+                wheels[i].setPower(currentMotorSpeed);
+            }
+            return;
+        }
+
+        for(int i = 0; i < wheels.length; ++i){
+            wheels[i].setPower(currentMotorSpeed);
+        }
+
+    }
+
+    public boolean isTaskDispatched(){
+        return dispatchedTask;
+    }
+
+}
