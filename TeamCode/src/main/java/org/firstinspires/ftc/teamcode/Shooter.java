1a32ed2800f736b14cb09e33b692f075632dcc2c
SNiranjanSS <saisainiranjan@gmail.com>
2026-09-12 22:01:27 -0500

    minor tweaks to shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -6,73 +6,54 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 //Step one
 @TeleOp(name = "Shooter")
-public class Shooter extends LinearOpMode {
+public class Shooter {
     public DcMotorEx shooter; //To get motor's extra features
     private double power = 0;
     Telemetry _telemetry;
+    Gamepad gamepad;
+    HardwareMap hardwareMap;
+    boolean isZero = false;
 
-    public Shooter(String shooterName, Telemetry telemetry){
+    public Shooter(String shooterName, Telemetry telemetry, HardwareMap h, Gamepad GameGamepad){
+        this.hardwareMap = h;
         this.shooter = hardwareMap.get(DcMotorEx.class, shooterName);
         this.shooter.setDirection(DcMotorSimple.Direction.FORWARD);
         this.shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         this._telemetry = telemetry;
+        this.gamepad = GameGamepad;
     }
 
-    public void run(){
-        //Raises power by 0.05 each time a is pressed
-        if (gamepad1.aWasPressed()) {
+    public void run() {
+        //Raises power by 0.05 each time 'a' is pressed
+        if (gamepad.aWasPressed()) {
             power = Math.min(1.0, power + 0.05);
             //Lowers power by 0.05 each time b is pressed
-        } else if (gamepad1.bWasPressed()) {
+        } else if (gamepad.bWasPressed()) { // don't NEED else if
             power = Math.max(0.0, power - 0.05);
         }
+        // might want to add an instant start. recommend: make a boolean named activated, separate to power
         //Instant stop
-        if (gamepad1.xWasPressed()) {
-            power = 0;
+        if (gamepad.xWasPressed()) {
+            isZero = !isZero;
         }
-        shooter.setPower(power);
+
+        if (isZero) {
+            shooter.setPower(0);
+        } else{
+            shooter.setPower(power);
+        }
         double rpm = shooter.getVelocity() * 60 / 28; //Reads speed
 
         //Display speed
         _telemetry.addData("Shooter power", "%.2f", power);
         _telemetry.addData("Shooter RPM", "%.0f", rpm);
-    }
-
-    @Override
-    public void runOpMode() {
-        //When driver is in init
-        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
-        //Step 3
-        shooter.setDirection(DcMotor.Direction.FORWARD);
-        //FLOAT allows for wheel to coast down to be gentle on gears.
-        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        //When driver presses start
-        waitForStart();
-        while (opModeIsActive()) {
-            //Raises power by 0.05 each time a is pressed
-            if (gamepad1.aWasPressed()) {
-                power = Math.min(1.0, power + 0.05);
-                //Lowers power by 0.05 each time b is pressed
-            } else if (gamepad1.bWasPressed()) {
-                power = Math.max(0.0, power - 0.05);
-            }
-            //Instant stop
-            if (gamepad1.xWasPressed()) {
-                power = 0;
-            }
-            shooter.setPower(power);
-            double rpm = shooter.getVelocity() * 60 / 28; //Reads speed
-
-            //Display speed
-            _telemetry.addData("Shooter power", "%.2f", power);
-            _telemetry.addData("Shooter RPM", "%.0f", rpm);
-            _telemetry.update();
-        }
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
@@ -5,18 +5,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.Shooter;
 
 //Step one
 @TeleOp(name = "Shooter")
 public class ShooterTest extends LinearOpMode {
     Shooter shooter;
+    DcMotor ex;
 
     @Override
     public void runOpMode() {
         //When driver is in init
-        shooter = new Shooter("shooter", telemetry);
-
+        shooter = new Shooter("shooter", telemetry, hardwareMap, gamepad1);
         waitForStart();
         while (opModeIsActive()) {
             shooter.run();
